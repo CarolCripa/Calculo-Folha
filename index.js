fc1b3481585278c5ea6d76b0ebafa6642e143345
Caroline Cripa <146898564+CarolCripa@users.noreply.github.com>
2025-05-06 17:57:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let salariobruto = parseFloat (input ('Informe o salário bruto:'));
+let salarioBruto = 6_000.00;//parseFloat(input('Informe o salário bruto:'));
 
-let inssFaixaInicio = 0;
-let inssFaixaFim = 1_518.00;
-let inssFaixaLiquota = 7.5;
+
+let inssFaixa1Inicio = 0;
+let inssFaixa1Fim = 1_518.00;
+let inssFaixa1Aliquota = 7.5;
 
 let faixa1 = {
     inicio: 0,
@@ -11,33 +12,37 @@ let faixa1 = {
 };
 
 let faixa2 = {
-    inicio: 1_518.00,
+    inicio: 1_518.01,
     fim: 2_793.88,
     aliquota: 0.09
 };
 
 let faixa3 = {
-    inicio: 2_793.88,
+    inicio: 2_793.89,
     fim: 4_190.83,
     aliquota: 0.12
 };
 
 let faixa4 = {
-    inicio: 4_190.83,
+    inicio: 4_190.84,
     fim: 8_157.81,
     aliquota: 0.14
 };
 
-let tabelaINSS = {faixa1, faixa2, faixa3, faixa4};
+
+let tabelaINSS = [faixa1, faixa2, 
+                faixa3, faixa4];
 
 console.table(tabelaINSS);
 
 let faixaEncontrada = {};
-for (let 1 = 0; i < tabelaINSS.length; i++) {
-    const faixa = tabelaINSS [i];
-    if (salarioBruto >= faixa.inicio &&
-        salarioBruto <= faixa.fim) {
-            faixaEncontrada = faixa;
-            break;
-        }
+for (let i = 0; i < tabelaINSS.length; i++) {
+    const faixa = tabelaINSS[i];
+    if(salarioBruto >= faixa.inicio &&
+       salarioBruto <= faixa.fim) {
+        faixaEncontrada = faixa; 
+        break; 
+       }    
 }
+
+console.log(faixaEncontrada);
